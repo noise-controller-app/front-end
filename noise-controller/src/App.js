6cc1787096bb5dd64341409ce8ae8ec1c6c396bf
Cole Gonzales <gcolegonzales003@gmail.com>
2019-09-19 18:53:46 -0500

    Added basic sign up form.

--- noise-controller/src/App.js
+++ noise-controller/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import Nav from './components/Nav';
-import UserForm from './components/Form';
+import UserForm from './components/UserForm';
 import './App.css';
 
 function App() {
