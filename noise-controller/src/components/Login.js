232a06e65512544a076b552fb8d1219200a69c76
Amber-Pittman <amber@amberpittman.com>
2019-09-19 21:06:16 -0400

    Wrapped Form with Formik

--- noise-controller/src/components/Login.js
+++ noise-controller/src/components/Login.js
@@ -1,28 +1,25 @@
 import React, { useState, useEffect } from 'react';
-import Formik, { Form, Field, withFormik } from 'formik';
+import { Formik, Form, Field, withFormik } from 'formik';
 import axios from 'axios';
-import styled from "styled-components";
 
 
 function LoginForm(){
-    const [ returnUser, setReturnUser ] = useState();
+    const [ returnUser, setReturnUser ] = useState([]);
 
     useEffect(() => {
-        // Do some stuff and thangs
+        // Do stuff and thangs
     }, [returnUser])
 
   return(
-      <div>
-        <h2>Making Classrooms Quiet Again!</h2>
-        <Form id="login">
-            <Field type="email" autoComplete="username" placeholder="Email" />
-            <Field type="password" autoComplete="current-password" placeholder="Password" />
-            <Field type="submit" value="Sign In!" />
-        </Form>
-      </div>
-  )
-
-}
+        <Formik>
+            <Form id='login'>
+                <Field type='text' autoComplete='name' placeholder='name' />
+                <Field type='email' autoComplete='username' placeholder='Email' />
+                <Field type='password' autoComplete='current-password' placeholder='Password' />
+                <Field type='submit' value='Sign In!' />
+            </Form>
+        </Formik>
+  )}
 
 
-export default LoginForm;
+  export default LoginForm;
