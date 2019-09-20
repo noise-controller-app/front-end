cf83fe14f98c2aa8d3f1c647039522d1029516b1
Amber-Pittman <amber@amberpittman.com>
2019-09-19 20:34:48 -0400

    Added Login component to App to render

--- noise-controller/src/App.js
+++ noise-controller/src/App.js
@@ -3,12 +3,15 @@ import Header from './components/Header';
 import Nav from './components/Nav';
 import UserForm from './components/Form';
 import './App.css';
+import LoginForm from './components/Login';
 
 function App() {
   return (
     <div>
       Hello World!
       <UserForm />
+
+      <LoginForm />
     </div>
   );
 }
