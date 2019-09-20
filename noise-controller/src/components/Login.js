9053a49ffe6cf766b7bac1a98619d2ba6fbcc13f
Amber-Pittman <amber@amberpittman.com>
2019-09-19 20:36:02 -0400

    Added Form and Field from Formik on Login component

--- noise-controller/src/components/Login.js
+++ noise-controller/src/components/Login.js
@@ -1,17 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import Formik, { Form, Field, withFormik } from 'formik';
 import axios from 'axios';
+import styled from "styled-components";
 
 
-function LoginForm()
+function LoginForm(){
+    const [ returnUser, setReturnUser ] = useState();
+
+    useEffect(() => {
+        // Do some stuff and thangs
+    }, [returnUser])
 
   return(
       <div>
         <h2>Making Classrooms Quiet Again!</h2>
-        <p>Email: </p>
-        <p>Password: </p>
+        <Form id="login">
+            <Field type="email" autoComplete="username" placeholder="Email" />
+            <Field type="password" autoComplete="current-password" placeholder="Password" />
+            <Field type="submit" value="Sign In!" />
+        </Form>
       </div>
   )
+
+}
 
 
 export default LoginForm;
