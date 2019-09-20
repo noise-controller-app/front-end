79f3e2dcc95f55064c56fdb4ba944d217e5e92a4
Benedictify <benedictify@gmail.com>
2019-09-19 20:36:39 -0500

    add link and route for /login, with dummy LoginForm file and component

--- noise-controller/src/App.js
+++ noise-controller/src/App.js
@@ -4,17 +4,20 @@ import { Route, Link } from "react-router-dom";
 import Header from "./components/Header";
 import Nav from "./components/Nav";
 import RegisterForm from "./components/RegisterForm";
+import LoginForm from "./components/LoginForm";
 import "./App.css";
 
 function App() {
   return (
     <div>
       <Link to="/">Home</Link>
-      {/* Adding a link to /login from the front page; later this could be moved into a navbar or elsewhere */}
+      {/* Adding a link to /register from the front page; later this could be moved into a navbar or elsewhere */}
       <Link to="/register">Register</Link>
+      <Link to="/login">Login</Link>
 
-      {/* RegisterForm will display at paths containing "/login" */}
+      {/* RegisterForm will display at paths containing "/register" */}
       <Route path="/register" component={RegisterForm} />
+      <Route path="/login" component={LoginForm} />
     </div>
   );
 }
