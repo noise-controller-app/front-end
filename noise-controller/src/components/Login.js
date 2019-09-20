8f78f9612afe3fff4a4886d024de294aa6d13034
Amber-Pittman <amber@amberpittman.com>
2019-09-19 20:03:51 -0400

    Started working on Login Form

---
+++ noise-controller/src/components/Login.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import Formik, { Form, Field, withFormik } from 'formik';
+import axios from 'axios';
+
+
+function LoginForm()
+
+  return(
+      <div>
+        <h2>Making Classrooms Quiet Again!</h2>
+        <p>Email: </p>
+        <p>Password: </p>
+      </div>
+  )
+
+
+export default LoginForm;
