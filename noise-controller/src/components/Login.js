42d553a57e747363a317a9fead36fb6570c7c62b
Amber-Pittman <amber@amberpittman.com>
2019-09-20 19:13:11 -0400

    Imported Yup and Styled-Components

--- noise-controller/src/components/Login.js
+++ noise-controller/src/components/Login.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { Formik, Form, Field, withFormik } from 'formik';
 import axios from 'axios';
+import * as Yup from 'yup';
+import styled from 'styled-components';
 
 
 function LoginForm(){
@@ -11,14 +13,14 @@ function LoginForm(){
     }, [returnUser])
 
   return(
-        <Formik>
+        <div>
             <Form id='login'>
                 <Field type='text' autoComplete='name' placeholder='name' />
                 <Field type='email' autoComplete='username' placeholder='Email' />
                 <Field type='password' autoComplete='current-password' placeholder='Password' />
                 <Field type='submit' value='Sign In!' />
             </Form>
-        </Formik>
+        </div>
   )}
 
 
