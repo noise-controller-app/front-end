8c014e2fa7b121f077e32f9b07148489293b4deb
Benedictify <benedictify@gmail.com>
2019-09-19 20:27:37 -0500

    add route to /login, links for home, login

--- noise-controller/src/App.js
+++ noise-controller/src/App.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import Header from './components/Header';
-import Nav from './components/Nav';
-import UserForm from './components/UserForm';
-import './App.css';
+import React from "react";
+import { Route, Link } from "react-router-dom";
+
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import UserForm from "./components/UserForm";
+import "./App.css";
 
 function App() {
   return (
     <div>
-      <UserForm />
+      <Link to="/">Home</Link>
+      {/* Adding a link to /login from the front page; later this could be moved into a navbar or elsewhere */}
+      <Link to="/login">Login</Link>
+
+      {/* UserForm will display at paths containing "/login" */}
+      <Route path="/login" component={UserForm} />
     </div>
   );
 }
