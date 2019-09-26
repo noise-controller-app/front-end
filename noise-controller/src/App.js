1f8938b1fc92f27b702c79876c105aac3a9004b9
Michael Hart <michael@mhart.dev>
2019-09-26 09:26:19 -0400

    Netlify and manifest tweaks

--- noise-controller/src/App.js
+++ noise-controller/src/App.js
@@ -1,57 +1,20 @@
 import React, { useState, useEffect } from "react";
-import { Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import TeacherPage from "./components/TeacherPage";
 import Forms from "./components/Forms";
 
 function App() {
-  const [second, setSecond] = useState(0);
-  const [minute, setMinute] = useState(0);
-  const [isActive, setIsActive] = useState(true);
-
-  // This useEffect works properly. However, we want this to be able to start and stop, not just
-  // run when the page starts. I am having a hard time figuring out how to run it through the button.
-  // The info has been passed down via props, but does not alter the state.
-
-  // useEffect(() => {
-  //   const intervalId = setInterval(() => {
-  //     if(second >= 59) {
-  //       setMinute(minute + 1);
-  //       setSecond(0);
-  //     } else {
-  //       setSecond(second + 1);
-  //     }
-  //   }, 1000);
-
-  //   return () => clearInterval(intervalId);
-  // }, [second]);
-
-  const toggleButton = () => {
-    console.log("Clicked!");
-    if (isActive) {
-      console.log("Active!");
-      const intervalId = setInterval(() => {
-        if (second >= 59) {
-          console.log("counting?");
-          setMinute(minute + 1);
-          setSecond(0);
-        } else {
-          setSecond(second + 1);
-        }
-      }, 1000);
-
-      return () => clearInterval(intervalId);
-    } else {
-      return;
-    }
-  };
-
   return (
-    <div>
-      <Route exact path="/" component={Forms} />
-      {/* <TeacherPage minute={minute} second={second} toggleButton={toggleButton} /> */}
-      <Route exact path="/teacher/:id" component={TeacherPage} />
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Forms} />
+          {/* <TeacherPage minute={minute} second={second} toggleButton={toggleButton} /> */}
+          <Route exact path="/teacher/:id" component={TeacherPage} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
